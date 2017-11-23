7e5b70d9d9161620afe4404ced16542287e8043e
Muhammad Salah <muhammad.hassan93@eng-st.cu.edu.eg>
2017-11-23 20:18:13 +0200

    Refining

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -8,21 +8,20 @@
 # Blockchain Weekends Program: Lab 1 "Blockchain Network Design"
 #
 
-### Before running this script make sure the Hyperledger fabric binaries are accessible in your PATH.
+### Before running this script make sure the Hyperledger fabric binaries are accessible in your PATH. Run it directly, because it is dependent on your path.
 cd ..
-rm -r ./crypto-config
+rm -r crypto-config
 
 cryptogen generate --config=./crypto-config.yaml
 
-rm -r ./channel-artifacts
+rm -r channel-artifacts
 
-mkdir ./channel-artifacts
+mkdir channel-artifacts
 
 
-export FABRIC_CFG_PATH=$PWD
+
+FABRIC_CFG_PATH=$PWD configtxgen -profile OrdererGenesis -outputBlock ./../channel-artifacts/genesis.block
+FABRIC_CFG_PATH=$PWD configtxgen -profile Channel -outputAnchorPeersUpdate ./../channel-artifacts/mailbox1MSPanchors.tx -channelID channel -asOrg mailbox1MSP
+FABRIC_CFG_PATH=$PWD configtxgen -profile Channel -outputAnchorPeersUpdate ./../channel-artifacts/mailbox2MSPanchors.tx -channelID channel -asOrg mailbox2MSP
 
 cd ./scripts
-
-configtxgen -profile OrdererGenesis -outputBlock ./../channel-artifacts/genesis.block
-configtxgen -profile Channel -outputAnchorPeersUpdate ./../channel-artifacts/mailbox1MSPanchors.tx -channelID channel -asOrg mailbox1MSP
-configtxgen -profile Channel -outputAnchorPeersUpdate ./../channel-artifacts/mailbox2MSPanchors.tx -channelID channel -asOrg mailbox2MSP
